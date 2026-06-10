13038481f3aaeb37f90273e1e87faa16f3648aab
David Melo <davidmelodesigner@gmail.com>
2026-06-10 13:20:26 -0300

    estudo server online

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const http = require("http");
+const WebSocket = require("ws");
+
+const callconfigs = require("./config");
+const callconfigs = require("./app/home.js");
+
+const app = express();
+
+
+app.get("/", (req, res) => {
+    homepage(req, res);
+});
+
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+wss.on("connection", (ws) => {
+
+    ws.on("message", (msg) => {
+        const data = JSON.parse(msg.toString());
+       
+
+        
+    });
+
+    
+
+});
+
+server.listen(process.env.PORT || 3000, () => {
+    console.log("Servidor online");
+});
