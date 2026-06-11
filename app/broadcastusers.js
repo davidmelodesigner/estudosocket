c031d7bdff1c58054e082e55e2e3fa1be314fae6
gamextore <54872302+davidmelodesigner@users.noreply.github.com>
2026-06-10 21:07:25 -0300

    Update broadcastusers.js

--- app/broadcastusers.js
+++ app/broadcastusers.js
@@ -1,4 +1,14 @@
+const players = {};
+
 module.exports = function broadcastusers(wss, ws, data) {
 
+    if (data.playerid) {
+        players[data.playerid] = data;
+    }
+
+    ws.send(JSON.stringify({
+        message: "allusers",
+        users: players
+    }));
 
 };
