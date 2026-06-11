c75017c693ce73ca792987bba6fb24aa948b0dfb
gamextore <54872302+davidmelodesigner@users.noreply.github.com>
2026-06-10 22:23:36 -0300

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const http = require("http");
 const WebSocket = require("ws");
 
-const callconfigs = require("./config");
 const homepage = require("./app/home.js");
 const startserver = require("./app/startserver.js");
 const playerupdate = require("./app/playerupdate.js");
@@ -19,40 +18,38 @@ app.get("/", (req, res) => {
 
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
-function broadcast(wss, msg) {
-    wss.clients.forEach(client => {
-        if (client.readyState === 1) {
-            client.send(JSON.stringify(msg));
-        }
-    });
-}
+
 wss.on("connection", (ws) => {
 
     ws.on("message", (msg) => {
         const data = JSON.parse(msg.toString());
 
-        if (data.message === "sendconnect") {
+        if (data.message == "sendconnect") {
 
-                if (connections[data.playerid]) {
-                    connections[data.playerid].terminate();
-                    delete connections[data.playerid];
-                    delete players[data.playerid];
-                }
-            
-                ws.playerid = data.playerid;
-            
-                connections[data.playerid] = ws;
-                players[data.playerid] = data;
-            
-                broadcast(wss, {
-                    message: "playerjoin",
-                    playerid: data.playerid
-                });
-            
-                startserver(ws, data, connections, players);
+            if (connections[data.playerid]) {
+                connections[data.playerid].terminate();
+                delete connections[data.playerid];
+                delete players[data.playerid];
             }
 
-        if (data.message === "playerupdate") {
+            ws.playerid = data.playerid;
+
+            connections[data.playerid] = ws;
+            players[data.playerid] = data;
+
+            wss.clients.forEach(client => {
+                if (client.readyState === 1) {
+                    client.send(JSON.stringify({
+                        message: "playerjoin",
+                        playerid: data.playerid
+                    }));
+                }
+            });
+
+            startserver(ws, data, connections, players);
+        }
+
+        if (data.message == "playerupdate") {
 
             connections[data.playerid] = ws;
             players[data.playerid] = data;
@@ -60,27 +57,32 @@ wss.on("connection", (ws) => {
             playerupdate(wss, ws, data, connections);
         }
 
-        if (data.message === "getallusers") {
+        if (data.message == "getallusers") {
             broadcastusers(wss, ws, data, connections);
         }
 
     });
 
+    // 👇 AQUI É O "connection close"
     ws.on("close", () => {
 
         const id = ws.playerid;
-    
+
         if (!id) return;
-    
+
         delete connections[id];
         delete players[id];
-    
-        broadcast(wss, {
-            message: "playerleave",
-            playerid: id
+
+        wss.clients.forEach(client => {
+            if (client.readyState === 1) {
+                client.send(JSON.stringify({
+                    message: "playerleave",
+                    playerid: id
+                }));
+            }
         });
-    
-        console.log("LEFT:", id);
+
+        console.log("SAIU:", id);
     });
 
 });
