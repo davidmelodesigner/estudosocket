cfe73cc0fb2c9aaa765c029857e7f66d5fa19e47
gamextore <54872302+davidmelodesigner@users.noreply.github.com>
2026-06-10 22:15:15 -0300

    Update server.js

--- server.js
+++ server.js
@@ -2,50 +2,56 @@ const express = require("express");
 const http = require("http");
 const WebSocket = require("ws");
 
+const callconfigs = require("./config");
+const homepage = require("./app/home.js");
+const startserver = require("./app/startserver.js");
+const playerupdate = require("./app/playerupdate.js");
+const broadcastusers = require("./app/broadcastusers.js");
+
+const players = require("./app/players");
+const connections = {};
+
 const app = express();
+
+
+app.get("/", (req, res) => {
+    homepage(req, res);
+});
 
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
-
-const players = {};
-const connections = {};
-
-function clearAll() {
-
-    wss.clients.forEach((ws) => {
-        try {
-            ws.terminate();
-        } catch (e) {}
-    });
-
-    for (const id in players) {
-        delete players[id];
-    }
-
-    for (const id in connections) {
-        delete connections[id];
-    }
-
-    console.log("CLEAN SERVER RESETADO");
-}
 
 wss.on("connection", (ws) => {
 
     ws.on("message", (msg) => {
         const data = JSON.parse(msg.toString());
 
-        if (data.message === "clear") {
-            clearAll();
+        if (data.message == "sendconnect") {
+
+            if (connections[data.playerid]) {
+                connections[data.playerid].terminate();
+                delete connections[data.playerid];
+                delete players[data.playerid];
+            }
+        
+            startserver(ws, data, connections, players);
+        }
+        if(data.message=="playerupdate"){
+            playerupdate(wss,ws,data,connections);
         }
 
-        if (data.message === "ping") {
-            ws.send(JSON.stringify({ message: "pong" }));
-        }
+        if(data.message=="getallusers"){
+            broadcastusers(wss,ws,data,connections);
+        } 
+        
+
+        
     });
 
-    ws.on("close", () => {});
+    
+
 });
 
-server.listen(3000, () => {
-    console.log("CLEAN SERVER ONLINE");
+server.listen(process.env.PORT || 3000, () => {
+    console.log("Servidor online");
 });
