18faf981a333311430bdd51947cdd9b2944507ea
gamextore <54872302+davidmelodesigner@users.noreply.github.com>
2026-06-17 17:37:14 -0300

    Update conectserver.js

--- conectserver.js
+++ conectserver.js
@@ -1,1 +1,11 @@
 
+module.exports = function conectserver(ws, data, wss) {
+
+    const payload = JSON.stringify({
+        message: "userdisconnect",
+        type:data.type
+        userid: userId
+    });
+
+  ws.send(JSON.stringify(payload));
+};
