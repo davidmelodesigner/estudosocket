5bf49d84754bbc36eced88d7321541998e7b1556
gamextore <54872302+davidmelodesigner@users.noreply.github.com>
2026-06-17 18:47:22 -0300

    Update getusers.js

--- getusers.js
+++ getusers.js
@@ -1,10 +1,15 @@
-
-module.exports = function getallusers(ws, data, wss,players) {
+module.exports = function getallusers(ws, data, wss, players) {
 
     const payload = JSON.stringify({
         message: "receiveusers",
-        users: players, 
+        users: players
     });
 
-    ws.send(payload);
+    wss.clients.forEach(client => {
+
+        if (client.readyState === WebSocket.OPEN) {
+            client.send(payload);
+        }
+
+    });
 };
