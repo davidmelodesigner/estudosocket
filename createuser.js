5da5c0fc3aeee489f7583e587cba17748426210a
gamextore <54872302+davidmelodesigner@users.noreply.github.com>
2026-06-18 20:46:39 -0300

    Update createuser.js

--- createuser.js
+++ createuser.js
@@ -1,19 +1,8 @@
-const { Pool } = require("pg");
-const evenconfig = require("./config.js");
 
-const pool = new Pool({
-    connectionString: evenconfig('payload'),
-    ssl: {
-        rejectUnauthorized: false
-    }
-});
 module.exports = function createUser(ws, data, wss)  {
-
-    const userId = data.userId;
-
      const payload = JSON.stringify({
         message: "userlogued",
-        userid: userId
+        userid: data
     });
     ws.send(payload);
 };
