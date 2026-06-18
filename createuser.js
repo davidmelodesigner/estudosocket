fcd7cc5fe52da40a22384ecf87dfdfa22be62a74
gamextore <54872302+davidmelodesigner@users.noreply.github.com>
2026-06-18 17:04:14 -0300

    Update createuser.js

--- createuser.js
+++ createuser.js
@@ -1,9 +1,7 @@
 module.exports = function createUser(ws, data, wss) {
 
     const payload = JSON.stringify({
-        message: "usercreated",
-        type: data.type,
-        userid: ws.userId
+        message: "usercreated"
     });
 
     ws.send(payload);
